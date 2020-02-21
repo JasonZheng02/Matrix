0d8b2dab06ef7c3094abe4c1d35506ae8c2b9e44
Jason Zheng <jzheng02@stuy.edu>
2020-02-21 05:37:46 -0500

    making pic

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from matrix import *
 screen = new_screen()
 color = [ 0, 0, 0 ]
 matrix = new_matrix(0,0)
+#stick figure
 add_edge(matrix, 10,5,0, 55,45,0 )
 add_edge(matrix, 85,5,0, 55,45,0 )
 add_edge(matrix, 55,45,0, 55,150,0 )
@@ -12,6 +13,11 @@ add_edge(matrix, 25,150,0, 85,150,0 )
 add_edge(matrix, 25,150,0, 25,210,0 )
 add_edge(matrix, 85,150,0, 85,210,0 )
 add_edge(matrix, 25,210,0, 85,210,0 )
+add_edge(matrix, 55,90,0, 20,120,0 )
+add_edge(matrix, 55,90,0, 90,120,0 )
+
+#sword
+add_edge(matrix, 75,130,0, 115,110,0 )
 
 draw_lines( matrix, screen, color )
 display(screen)
