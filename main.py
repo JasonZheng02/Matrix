832e0191ac2624f7a530be1f9fb62e57ee04f620
Jason Zheng <jzheng02@stuy.edu>
2020-02-21 04:46:14 -0500

    making pic

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ from matrix import *
 
 screen = new_screen()
 color = [ 255, 255, 0 ]
-matrix = new_matrix(0,0)
+matrixY = new_matrix(0,0)
+matrixR = new_matrix(0,0)
 
 x = 200
 while x < 250:
-    add_edge(matrix, x,300,0, x,350,0)
+    add_edge(matrixY, x,300,0, x,350,0)
     x = x + 1
 
 x = 150
 while x < 300:
-    add_edge(matrix, x,100,0, x,300,0)
+    add_edge(matrixY, x,100,0, x,300,0)
     x = x + 1
-
-draw_lines( matrix, screen, color )
 
 color = [ 255, 0, 0 ]
 x = 210
 while x < 220:
-    add_edge(matrix, x,340,0, x,350,0)
+    add_edge(matrixR, x,340,0, x,350,0)
     x = x + 1
 
-draw_lines( matrix, screen, color )
+draw_lines( matrixY, screen, color )
+draw_lines( matrixR, screen, color )
 display(screen)
