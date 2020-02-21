7c2812352d2f1e8c2ef9e39360ee82fc0d8f7786
Jason Zheng <jzheng02@stuy.edu>
2020-02-21 04:36:58 -0500

    making pic

--- main.py
+++ main.py
@@ -11,5 +11,10 @@ while x < 300:
     add_edge(matrix, x,300,0, x,400,0)
     x = x + 1
 
+x = 100
+while x < 400:
+    add_edge(matrix, x,100,0, x,300,0)
+    x = x + 1
+
 draw_lines( matrix, screen, color )
 display(screen)
