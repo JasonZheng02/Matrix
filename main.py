ec5e1b64ea65fdaeb4f81f7964da910c798964f5
Jason Zheng <jzheng02@stuy.edu>
2020-02-21 04:21:27 -0500

    testing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from matrix import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix(0,0)
+add_edge(matrix, 100,100,0, 200,400,0)
 
 draw_lines( matrix, screen, color )
 display(screen)
